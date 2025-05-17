238bd74097932c38c25743285ca4735cf3850ee0
musclefrog <rlatndls113@gmail.com>
2025-05-17 23:42:13 +0900

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header style={{ padding: '1rem', fontWeight: 'bold' }}>
+        Briefly
+      </header>
+      <main style={{ padding: '1rem' }}>
+        <p> 관심사를 선택해 주세요</p>
+        {/* 여기에 버튼이나 뉴스 리스트 들어감 */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
